2fa92c3872b0351f82868fecbb12f03e993c950e
Pablos <pablos@list.ru>
2022-05-02 23:58:54 +0300

    idea for lines deletion is ready

--- My_tetris/Frame.cs
+++ My_tetris/Frame.cs
@@ -9,13 +9,15 @@ namespace My_tetris
     internal class Frame
     {
         List<Figure> wallList;
-        public int w;
+        public int wide;
+        public int high;
         public List<Point> lineDownPoints;
         public List<Point> upLinePoints;
         public Frame(int width, int height)
         {
             wallList = new List<Figure>();
-            w = width;
+            wide = width;
+            high = height;
             HorizLine lineUp = new (0, width, 0, '-');
             HorizLine lineDown = new (0, width, height, '-');
             VertLine lineLeft = new (0, 0, height, '|');
--- My_tetris/Program.cs
+++ My_tetris/Program.cs
@@ -1,38 +1,47 @@
 ﻿My_tetris.Frame frame = new (20, 28);
 frame.Draw();
 
-Random rnd = new();
-int dice = rnd.Next(1, 8);
-
-My_tetris.Block block = new (dice, frame.w);
+int dice = new Random().Next(1, 8);
+My_tetris.Block block = new (dice, frame.wide);
 block.Draw();
 
 List<My_tetris.Point> playGround = new();
 
 while (true)
 {
-    //    if (точек в линии == ширине frame.width) удалить линию
-
-
     if (block.Fallen(playGround, frame.lineDownPoints))
     {
-        if (block.BreakingUpLine(frame.upLinePoints)) break; // Геймовер
+        // Добавить точки блока в массу на игровом поле
         playGround.AddRange(block.ls);
-        dice = rnd.Next(1, 8);
-        My_tetris.Block block1 = new(dice, frame.w);
+
+        // линия - список точек
+        // список линий
+        //    if (точек в линии == ширине frame.wide) удалить линию, сместить оставшиеся
+
+        // Отрисовываем ещё раз всю массу на поле
+        foreach (My_tetris.Point p in playGround) p.Draw();
+
+        // Геймовер
+        if (block.BreakingUpLine(frame.upLinePoints)) break;
+
+        // Новый блок
+        dice = new Random().Next(1, 8);
+        My_tetris.Block block1 = new(dice, frame.wide);
         block = block1;
         block.Draw();
     }
 
+    // Перемещения стрелочками
     if (Console.KeyAvailable)
     {
         ConsoleKeyInfo key = Console.ReadKey();
-        block.DirectionListener(key.Key, frame.w, block.f);
+        block.DirectionListener(key.Key, frame.wide, block.f);
     }
 
-    block.Move("down", frame.w);
-    Thread.Sleep(250);
+    block.Move("down", frame.wide);
 
+    // Скорость падения блоков
+    Thread.Sleep(250);
 }
 
 Console.SetCursorPosition(30, 12);
